c3a49fd115481fc176adfa117190e6b382932e7e
chaitanya-uniyal <chaitanyauniyal70@gmail.com>
2024-02-10 18:28:25 +0530

    updated frontend

--- server/index.js
+++ server/index.js
@@ -1,55 +1,42 @@
 import express from "express";
-import helmet, { crossOriginResourcePolicy } from "helmet";
 import bodyParser from "body-parser";
+import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
+import helmet from "helmet";
 import morgan from "morgan";
-import mongoose from "mongoose";
+import kpiRoutes from "./routes/kpi.js";
+import productRoutes from "./routes/product.js";
+import transactionRoutes from "./routes/transaction.js";
+import KPI from "./models/KPI.js";
+import Product from "./models/Product.js";
+import Transaction from "./models/Transaction.js";
+import { kpis, products, transactions } from "./data/data.js";
 
-
-/* configuration */
-
+/* CONFIGURATIONS */
 dotenv.config();
 const app = express();
 app.use(express.json());
-app.use(helmet);
-app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin"}));
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
+/* ROUTES */
+app.use("/kpi", kpiRoutes);
+app.use("/product", productRoutes);
+app.use("/transaction", transactionRoutes);
 
-
-
-/* MONGOOSE setup */
-
+/* MONGOOSE SETUP */
 const PORT = process.env.PORT || 9000;
-// var options = {
-//     useMongoClient: true,
-//     autoIndex: false, // Don't build indexes
-//     reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
-//     reconnectInterval: 500, // Reconnect every 500ms
-//     poolSize: 10, // Maintain up to 10 socket connections
-//     // If not connected, return errors immediately rather than waiting for reconnect
-//     bufferMaxEntries: 0
-//   };
-// mongoose.connect(process.env.MONGO_URL, options);
-var options = {
-    useMongoClient: true,
-    autoIndex: false, // Don't build indexes
-    reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
-    reconnectInterval: 500, // Reconnect every 500ms
-    poolSize: 10, // Maintain up to 10 socket connections
-    // If not connected, return errors immediately rather than waiting for reconnect
-    bufferMaxEntries: 0
-  };
-  mongoose.connect(process.env.MONGO_URL, options);
-  // connection.openUri(process.env.MONGO_URL);
-  
-  // mongoose.connect(process.env.MONGO_URL)
-
-// .then(()=>{
-//     app.listen(PORT ,()=> console.log(`Server Port: ${PORT}`));
-
-//     }).catch((error) => console.log(`${error} did not connect`));
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(async () => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+  })
+  .catch((error) => console.log(`${error} did not connect`));
